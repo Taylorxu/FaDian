44cfc8b8caccd13f1a8a15c1969191d4c1bf0515
Taylorxu <zhiguang_xu@live.cn>
2018-03-26 19:45:37 +0800

    经济指标 列表联动

--- app/src/main/java/com/powerge/wise/powerge/otherPages/JingJiZhiBiaoActivity.java
+++ app/src/main/java/com/powerge/wise/powerge/otherPages/JingJiZhiBiaoActivity.java
@@ -6,11 +6,17 @@ import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.widget.AbsListView;
+import android.widget.BaseAdapter;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.SimpleAdapter;
 
 import com.hyphenate.util.DensityUtil;
@@ -42,6 +48,8 @@ import rx.Subscriber;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.schedulers.Schedulers;
 
+import static android.support.v7.widget.RecyclerView.SCROLL_STATE_IDLE;
+
 public class JingJiZhiBiaoActivity extends AppCompatActivity {
     public static void start(Context context) {
         Intent starter = new Intent(context, JingJiZhiBiaoActivity.class);
@@ -58,15 +66,58 @@ public class JingJiZhiBiaoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_jing_ji_zhi_biao);
         binding.title.setText(getResources().getStringArray(R.array.item_name_array)[2]);
-        initView();
         getZBNameList();
         getZBValueList();
+
     }
 
-    private void initView() {
-        binding.zhiBiaoListId.setLayoutManager(new LinearLayoutManager(getBaseContext()));
-        binding.zhiBiaoListId.setAdapter(nameListXAdapter);
 
+    private void setScrollListenter() {
+        binding.zhiBiaoListId.setOnScrollListener(new MyScrollListener());
+        for (int i = 0; i < binding.contentJiZu.getChildCount(); i++) {
+            LinearLayout view = (LinearLayout) binding.contentJiZu.getChildAt(i);
+            ListView listview = (ListView) view.getChildAt(2);
+            listview.setOnScrollListener(new MyScrollListener());
+        }
+    }
+
+
+    private class MyScrollListener implements AbsListView.OnScrollListener {
+
+
+        @Override
+        public void onScrollStateChanged(AbsListView view, int scrollState) {
+// 关键代码
+            if (scrollState == SCROLL_STATE_IDLE
+                    || scrollState == SCROLL_STATE_TOUCH_SCROLL) {
+                View subView = view.getChildAt(0);
+                if (subView != null) {
+                    final int top = subView.getTop();
+                    final int position = view.getFirstVisiblePosition();
+                    for (int i = 0; i < binding.contentJiZu.getChildCount(); i++) {
+                        LinearLayout view1 = (LinearLayout) binding.contentJiZu.getChildAt(i);
+                        ListView listview = (ListView) view1.getChildAt(2);
+                        listview.setSelectionFromTop(position, top);
+                    }
+                    binding.zhiBiaoListId.setSelectionFromTop(position,top);
+                }
+            }
+        }
+
+        @Override
+        public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+            // 关键代码
+            View subView = view.getChildAt(0);
+            if (subView != null) {
+                final int top = subView.getTop();
+                for (int i = 0; i < binding.contentJiZu.getChildCount(); i++) {
+                    LinearLayout view1 = (LinearLayout) binding.contentJiZu.getChildAt(i);
+                    ListView listview = (ListView) view1.getChildAt(2);
+                    listview.setSelectionFromTop(firstVisibleItem, top);
+                }
+                binding.zhiBiaoListId.setSelectionFromTop(firstVisibleItem,top);
+            }
+        }
     }
 
     public void onClick(View view) {
@@ -101,7 +152,10 @@ public class JingJiZhiBiaoActivity extends AppCompatActivity {
 
                     @Override
                     public void onNext(List<ZhiBaioNameBean> list) {
-                        if (list.size() > 0) nameListXAdapter.setList(list);
+                        if (list.size() > 0) {
+                            Adapter adapter = new Adapter(list);
+                            binding.zhiBiaoListId.setAdapter(adapter);
+                        }
                     }
                 });
 
@@ -144,11 +198,91 @@ public class JingJiZhiBiaoActivity extends AppCompatActivity {
           /*  LinearLayout.LayoutParams param = new LinearLayout.LayoutParams(width1, LinearLayout.LayoutParams.WRAP_CONTENT );
             view.setLayoutParams(param);*/
             ItemJiZuZhiBiaoBinding itemBiaoBinding = DataBindingUtil.bind(view.getRootView());
-            itemBiaoBinding.zbValueList.setLayoutManager(new LinearLayoutManager(getBaseContext()));
-            itemBiaoBinding.zbValueList.setAdapter(valueListXAdapter);
-            valueListXAdapter.setList(list.get(i).getDetails());
+            itemBiaoBinding.zbValueList.setAdapter(new Adapter1(list.get(i).getDetails()));
             itemBiaoBinding.setItemZhiBiao(list.get(i));
             binding.contentJiZu.addView(view);
         }
+
+        setScrollListenter();
+
     }
+
+
+    ItemZbNameListBinding nameListBinding;
+    ItemZbValueListBinding valueListBinding;
+
+    class Adapter extends BaseAdapter {
+        List<ZhiBaioNameBean> list;
+
+        public Adapter(List<ZhiBaioNameBean> list) {
+            this.list = list;
+        }
+
+        @Override
+        public int getCount() {
+            return list.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return list.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null) {
+                nameListBinding = DataBindingUtil.inflate(LayoutInflater.from(parent.getContext()), R.layout.item_zb_name_list, parent, false);
+                convertView = nameListBinding.getRoot();
+                convertView.setTag(nameListBinding);
+            } else {
+                nameListBinding = (ItemZbNameListBinding) convertView.getTag();
+            }
+            nameListBinding.setData(list.get(position));
+            return convertView;
+        }
+    }
+
+
+    class Adapter1 extends BaseAdapter {
+        List<ZhiBIaoValueBean.DetailsBean> list;
+
+        public Adapter1(List<ZhiBIaoValueBean.DetailsBean> list) {
+            this.list = list;
+        }
+
+        @Override
+        public int getCount() {
+            return list.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return list.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null) {
+                valueListBinding = DataBindingUtil.inflate(LayoutInflater.from(parent.getContext()), R.layout.item_zb_value_list, parent, false);
+                convertView = valueListBinding.getRoot();
+                convertView.setTag(valueListBinding);
+            } else {
+                valueListBinding = (ItemZbValueListBinding) convertView.getTag();
+            }
+            valueListBinding.setData(list.get(position));
+            return convertView;
+        }
+    }
+
+
 }
