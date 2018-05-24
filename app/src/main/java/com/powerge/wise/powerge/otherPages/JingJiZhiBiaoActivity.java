a038deb72472c728585a5bccd09ba1f424c2c52d
Taylorxu <zhiguang_xu@live.cn>
2018-05-24 15:34:34 +0800

    经济指标 数据加载 体验

--- app/src/main/java/com/powerge/wise/powerge/otherPages/JingJiZhiBiaoActivity.java
+++ app/src/main/java/com/powerge/wise/powerge/otherPages/JingJiZhiBiaoActivity.java
@@ -1,10 +1,12 @@
 package com.powerge.wise.powerge.otherPages;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.content.Context;
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,9 +19,7 @@ import android.widget.ListView;
 import com.powerge.wise.basestone.heart.network.FlatMapResponse;
 import com.powerge.wise.basestone.heart.network.FlatMapTopRes;
 import com.powerge.wise.basestone.heart.network.ResultModel;
-import com.powerge.wise.basestone.heart.ui.XAdapter;
 import com.powerge.wise.basestone.heart.util.DensityUtil;
-import com.powerge.wise.powerge.BR;
 import com.powerge.wise.powerge.R;
 import com.powerge.wise.powerge.bean.User;
 import com.powerge.wise.powerge.bean.ZhiBIaoValueBean;
@@ -32,6 +32,7 @@ import com.powerge.wise.powerge.databinding.ActivityJingJiZhiBiaoBinding;
 import com.powerge.wise.powerge.databinding.ItemJiZuZhiBiaoBinding;
 import com.powerge.wise.powerge.databinding.ItemZbNameListBinding;
 import com.powerge.wise.powerge.databinding.ItemZbValueListBinding;
+import com.powerge.wise.powerge.helper.EEMsgToastHelper;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -47,16 +48,12 @@ public class JingJiZhiBiaoActivity extends AppCompatActivity {
     }
 
     ActivityJingJiZhiBiaoBinding binding;
-    List<ZhiBaioNameBean> baioNameList = new ArrayList<>();
-    XAdapter<ZhiBaioNameBean, ItemZbNameListBinding> nameListXAdapter = new XAdapter.SimpleAdapter<>(BR.data, R.layout.item_zb_name_list);
-    XAdapter<ZhiBIaoValueBean.DetailsBean, ItemZbValueListBinding> valueListXAdapter = new XAdapter.SimpleAdapter<>(BR.data, R.layout.item_zb_value_list);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_jing_ji_zhi_biao);
         binding.title.setText(getResources().getStringArray(R.array.item_name_array)[2]);
-//        getZBNameList();
         getZBValueList();
 
     }
@@ -116,41 +113,6 @@ public class JingJiZhiBiaoActivity extends AppCompatActivity {
         }
     }
 
-    /**
-     * 获取指标名称列表
-     */
-    private void getZBNameList() {
-        ZhiBaioNameBean nameBean = new ZhiBaioNameBean();
-        nameBean.setNameSpace(BaseUrl.NAMESPACE_P);
-        nameBean.setUserName(User.getCurrentUser().getName());
-        RequestEnvelope.getRequestEnvelope().setBody(new RequestBody<>(nameBean));
-        ApiService.Creator.get().queryEconomicIndicatorsList(RequestEnvelope.getRequestEnvelope())
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .flatMap(new FlatMapResponse<ResultModel<List<ZhiBaioNameBean>>>())
-                .flatMap(new FlatMapTopRes<List<ZhiBaioNameBean>>())
-                .subscribe(new Subscriber<List<ZhiBaioNameBean>>() {
-                    @Override
-                    public void onCompleted() {
-
-                    }
-
-                    @Override
-                    public void onError(Throwable e) {
-
-                    }
-
-                    @Override
-                    public void onNext(List<ZhiBaioNameBean> list) {
-                        if (list.size() > 0) {
-                            Adapter adapter = new Adapter(list);
-                            binding.zhiBiaoListId.setAdapter(adapter);
-                        }
-                    }
-                });
-
-    }
-
     private void getZBValueList() {
         ZhiBIaoValueBean valueBean = new ZhiBIaoValueBean();
         valueBean.setNameSpace(BaseUrl.NAMESPACE_P);
@@ -169,7 +131,8 @@ public class JingJiZhiBiaoActivity extends AppCompatActivity {
 
                     @Override
                     public void onError(Throwable e) {
-
+                        EEMsgToastHelper.newInstance().selectWitch(e.getMessage());
+                        crossfade();
                     }
 
                     @Override
@@ -177,6 +140,7 @@ public class JingJiZhiBiaoActivity extends AppCompatActivity {
                         if (list.size() > 0) {
                             createZBValueView(list);
                             createZBNameList(list.get(0).getDetails());
+                            crossfade();
                         }
                     }
                 });
@@ -201,7 +165,7 @@ public class JingJiZhiBiaoActivity extends AppCompatActivity {
 
     private void createZBValueView(List<ZhiBIaoValueBean> list) {
         WindowManager wm1 = this.getWindowManager();
-        int width1 = wm1.getDefaultDisplay().getWidth() / 2 - DensityUtil.dip2px(this,50);
+        int width1 = wm1.getDefaultDisplay().getWidth() / 2 - DensityUtil.dip2px(this, 50);
         for (int i = 0; i < list.size(); i++) {
             LinearLayout view = (LinearLayout) LayoutInflater.from(this).inflate(R.layout.item_ji_zu_zhi_biao, binding.contentJiZu, false);
             LinearLayout.LayoutParams param = new LinearLayout.LayoutParams(width1, LinearLayout.LayoutParams.WRAP_CONTENT);
@@ -216,6 +180,21 @@ public class JingJiZhiBiaoActivity extends AppCompatActivity {
 
     }
 
+
+    public void crossfade() {
+        binding.llLayoutContent.setAlpha(0f);
+        binding.llLayoutContent.setVisibility(View.VISIBLE);
+        binding.llLayoutContent.animate().alpha(1f)
+                .setDuration(1000);
+        binding.flLayoutProgress.animate().alpha(0f)
+                .setDuration(1000)
+                .setListener(new AnimatorListenerAdapter() {
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        binding.flLayoutProgress.setVisibility(View.GONE);
+                    }
+                });
+    }
 
     ItemZbNameListBinding nameListBinding;
     ItemZbValueListBinding valueListBinding;
